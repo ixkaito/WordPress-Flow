1d21928b1e8cb808e9e39cfa9df13f1fc0186452
KITE <ixkaito@gmail.com>
2014-09-30 08:56:56 +0900

    Use gulp-haml

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var haml = require('gulp-haml');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
@@ -7,53 +8,62 @@ var compass = require('gulp-compass');
 var browser = require('browser-sync');
 
 gulp.task('browser', function() {
-	browser({
-		server: {
-			baseDir: './'
-		}
-	});
+  browser({
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+gulp.task('haml', function() {
+  gulp.src('assets/haml/**/*haml')
+    .pipe(haml({
+      compiler: 'visionmedia'
+    }))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('html', function() {
-	gulp.src('./**/*.html')
-		.pipe(plumber())
-		.pipe(browser.reload({stream:true}));
+  gulp.src('./**/*.html')
+    .pipe(plumber())
+    .pipe(browser.reload({stream:true}));
 });
 
 gulp.task('sass', function() {
-	gulp.src('assets/sass/**/*scss')
-		.pipe(plumber())
-		.pipe(sass())
-		.pipe(autoprefixer())
-		.pipe(gulp.dest('./css'))
-		.pipe(browser.reload({stream:true}));
+  gulp.src('assets/sass/**/*scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./css'))
+    .pipe(browser.reload({stream:true}));
 });
 
 gulp.task('js', function() {
-	gulp.src('assets/js/**/*.js')
-		.pipe(plumber())
-		// .pipe(uglify())
-		// .pipe(gulp.dest('assets/js/min'))
-		.pipe(browser.reload({stream:true}));
+  gulp.src('assets/js/**/*.js')
+    .pipe(plumber())
+    // .pipe(uglify())
+    // .pipe(gulp.dest('assets/js/min'))
+    .pipe(browser.reload({stream:true}));
 });
 
 gulp.task('compass', function(){
-	gulp.src('assets/sass/**/*.scss')
-		.pipe(plumber())
-		.pipe(compass({
-			config_file: 'config.rb',
-			// comments: false,
-			css: 'assets/css/',
-			sass: 'assets/sass/'
-		}))
-		.pipe(browser.reload({stream:true}));
+  gulp.src('assets/sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(compass({
+      config_file: 'config.rb',
+      // comments: false,
+      css: 'assets/css/',
+      sass: 'assets/sass/'
+    }))
+    .pipe(browser.reload({stream:true}));
 });
 
 gulp.task('watch', function() {
-	// gulp.watch('sass/**/*.scss', ['sass']);
-	gulp.watch('assets/js/**/*.js', ['js']);
-	gulp.watch('assets/sass/**/*.scss', ['compass']);
-	gulp.watch('./**/*.html', ['html']);
+  // gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('assets/haml/**/*.haml', ['haml']);
+  gulp.watch('assets/js/**/*.js', ['js']);
+  gulp.watch('assets/sass/**/*.scss', ['compass']);
+  gulp.watch('./**/*.html', ['html']);
 });
 
 gulp.task('default', ['browser', 'watch']);
